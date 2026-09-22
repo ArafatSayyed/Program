df24265d7f403ced1cb882fbfa287033369ee40a
Arafat Sayyed <arafatsayyed07@gmail.com>
2026-09-22 15:36:49 +0530

    Time: 0 ms (100%), Space: 43.6 MB (39.82%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -3,35 +3,61 @@ class Solution
     public void sortColors(int[] nums)
     {
         int n = nums.length;
-        int NoZ = 0;
-        int NoO = 0;
-        int NoT = 0;
-        for (int ele : nums)
+        int mid = 0;
+        int low = 0;
+        int high = n - 1;
+        while (mid <= high)
         {
-            if (ele == 0)
+            if (nums[mid] == 0)
             {
-                NoZ++;
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++; 
             }
-            if (ele == 1)
+            else if (nums[mid] == 1)
             {
-                NoO++;
+                mid++;
             }
-            if (ele == 2)
+            else
             {
-                NoT++;
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--; 
             }
         }
-        for (int i = 0; i < NoZ; i++)
-        {
-            nums[i] = 0;
-        }
-        for (int i = NoZ; i < NoZ + NoO; i++)
-        {
-            nums[i] = 1;
-        }
-        for (int i = NoZ + NoO; i < n; i++)
-        {
-            nums[i] = 2;
-        }
+        // int n = nums.length;
+        // int NoZ = 0;
+        // int NoO = 0;
+        // int NoT = 0;
+        // for (int ele : nums)
+        // {
+        //     if (ele == 0)
+        //     {
+        //         NoZ++;
+        //     }
+        //     if (ele == 1)
+        //     {
+        //         NoO++;
+        //     }
+        //     if (ele == 2)
+        //     {
+        //         NoT++;
+        //     }
+        // }
+        // for (int i = 0; i < NoZ; i++)
+        // {
+        //     nums[i] = 0;
+        // }
+        // for (int i = NoZ; i < NoZ + NoO; i++)
+        // {
+        //     nums[i] = 1;
+        // }
+        // for (int i = NoZ + NoO; i < n; i++)
+        // {
+        //     nums[i] = 2;
+        // }
     }
 }
