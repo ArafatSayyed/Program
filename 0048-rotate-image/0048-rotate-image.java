9ac22fe35f1f30e2c64d9d329bbd9f4cb4a5785b
Arafat Sayyed <arafatsayyed07@gmail.com>
2026-09-24 03:22:15 +0530

    Time: 0 ms (100%), Space: 44.1 MB (16.69%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,29 @@
+class Solution
+{
+    public void rotate(int[][] arr)
+    {
+        int m = arr.length;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = i; j < m; j++)
+            {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < m; i++)
+        {
+            int a = 0;
+            int b = m - 1;
+            while (a < b)
+            {
+                int temp = arr[i][a];
+                arr[i][a] = arr[i][b];
+                arr[i][b] = temp;
+                a++;
+                b--;
+            }
+        }
+    }
+}
